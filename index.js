eaf8cc471662f98e7c28b5494f7c11cbdc8e7e32
Jelle Nijhof <j.nijhof@code14.nl>
2024-02-21 18:35:16 +0100

    new version

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ const chalk_1 = __importDefault(require("chalk"));
 const key = optimist_1.argv.key;
 const env = optimist_1.argv.env;
 const filename = optimist_1.argv.filename;
+const inputFilename = optimist_1.argv.inputFilename;
 const force = optimist_1.argv.force;
 const cipher = (_a = optimist_1.argv.cipher) !== null && _a !== void 0 ? _a : 'aes-256-cbc';
 process.on('uncaughtException', (error) => {
@@ -30,28 +31,27 @@ if (!command) {
     }
     throw new Error(`Command ${commandKey} not found`);
 }
-command(key, env, filename, force);
-function encryptCommand(key, env, filename, force) {
+command(key, env, filename, force, inputFilename);
+function encryptCommand(key, env, filename, force, inputFilename) {
     if (!key) {
         key = generateKey();
         log('info', 'Generated key: base64:' + Buffer.from(key).toString('base64'));
     }
-    const envFile = getEnvFileName(env);
+    const envFile = inputFilename !== null && inputFilename !== void 0 ? inputFilename : getEnvFileName(env);
     var data = getFile(envFile);
     var encrypted = encrypt(data, parseKey(key));
     writeFile(envFile + '.encrypted', encrypted, force);
     log('success', 'Encrypted to ' + envFile + '.encrypted');
 }
-function decryptCommand(key, env, filename, force) {
-    var _a;
+function decryptCommand(key, env, filename, force, inputFilename) {
     if (!key) {
         throw new Error('Key is required');
     }
-    const envFile = getEnvFileName(env);
+    const envFile = inputFilename !== null && inputFilename !== void 0 ? inputFilename : getEnvFileName(env);
     var data = getFile(envFile + '.encrypted');
     var decrypted = decrypt(data, parseKey(key));
     writeFile(filename !== null && filename !== void 0 ? filename : envFile, decrypted, force);
-    log('success', (_a = 'Decrypted to ' + filename) !== null && _a !== void 0 ? _a : envFile);
+    log('success', 'Decrypted to ' + (filename !== null && filename !== void 0 ? filename : envFile));
 }
 function log(level, message) {
     const timestamp = new Date().toISOString();
